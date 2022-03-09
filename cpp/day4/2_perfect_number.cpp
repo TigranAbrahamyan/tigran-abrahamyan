8ad82cf70d36d245ef2acdd1d712d2ca5ffe3911
TigranAbrahamyan <deulin666@gmail.com>
2022-03-09 18:24:57 +0400

    cpp homework day4, (perfect_number) exercise

---
+++ cpp/day4/2_perfect_number.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+/*
+  perfect number - if sum of number divisors equal to number it is a perfect number
+**/
+
+void isNumberPerfect(int n) {
+  int sum = 0;
+
+  for (int i = 1; i < n; i++) {
+    if (n % i == 0) {
+      sum = sum + i;
+    }
+  }
+
+  if (sum == n) {
+    cout << "Number is perfect";
+  } else {
+    cout << "Number is not perfect";
+  }
+}
+
+int main() {
+  int n;
+
+  cout << "Write a non-negative number: ";
+  cin >> n;
+
+  while (n <= 0) {
+    cout << "Error! write again: ";
+    cin >> n;
+  }
+
+  isNumberPerfect(n);
+  cout << endl;
+
+  return 0;
+}
