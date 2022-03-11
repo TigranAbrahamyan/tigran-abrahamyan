92def6be49cf7cc2c988d17ca76cf10a668a805f
TigranAbrahamyan <deulin666@gmail.com>
2022-03-11 21:50:37 +0400

    cpp homework day6, (two_matrix_sum) exercise

---
+++ cpp/day6/5_two_matrix_sum.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int randomNumberInRange(int max, int min) {
+  return (rand() % (max + 1) - min) + min;
+}
+
+int main() {
+  srand(time(0));
+  const int min = 1;
+  const int max = 9;
+
+  const int size = 4;
+  int firstArray[size][size] = {};
+  int secondArray[size][size] = {};
+  int resultArray[size][size] = {};
+
+  cout << "First" << endl;
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      firstArray[i][j] = randomNumberInRange(max, min);
+      cout << firstArray[i][j] << " ";
+    }
+
+    cout << endl;
+  }
+
+  cout << endl;
+  cout << "Second" << endl;
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      secondArray[i][j] = randomNumberInRange(max, min);
+      cout << secondArray[i][j] << " ";
+    }
+
+    cout << endl;
+  }
+
+  cout << endl;
+  cout << "Result" << endl;
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      resultArray[i][j] = firstArray[i][j] + secondArray[i][j];
+      cout << resultArray[i][j] << " ";
+    }
+
+    cout << endl;
+  }
+
+  return 0;
+}
