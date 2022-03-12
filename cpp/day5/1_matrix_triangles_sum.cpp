19bf8347c07e46dcfa1330e3cd8bfdd1bf131260
TigranAbrahamyan <deulin666@gmail.com>
2022-03-12 20:08:10 +0400

    day5 matrix triangles sum updated

--- cpp/day5/1_matrix_triangles_sum.cpp
+++ cpp/day5/1_matrix_triangles_sum.cpp
@@ -6,13 +6,14 @@ using namespace std;
 
 int main() {
   srand(time(0));
-  const int arraySize = 3;
   const int min = 1;
   const int max = 9;
+
+  const int arraySize = 4;
   int array[arraySize][arraySize] = {};
 
-  const int trianglesCountIncludingDiagonals = 5;
-  int trianglesSumArray[trianglesCountIncludingDiagonals] = {};
+  const int trianglesCount = 8;
+  int trianglesSumArray[trianglesCount] = {};
   int trianglesSumResult = 0;
 
   for (int i = 0; i < arraySize; i++) {
@@ -46,20 +47,38 @@ int main() {
         trianglesSumArray[3] += array[row][column];
       }
 
-      // exclude numbers on diagonals and get numbers in non-diagonals
-      if ((row != column) && (row + column) != (arraySize - 1)) {
+      // top triangle
+      if (row < column && row + column + 1 < arraySize) {
         trianglesSumArray[4] += array[row][column];
+      }
+
+      // left triangle
+      if (row > column && row + column + 1 < arraySize) {
+        trianglesSumArray[5] += array[row][column];
+      }
+
+      // right triangle
+      if (row < column && arraySize - row - 1 < column) {
+        trianglesSumArray[6] += array[row][column];
+      }
+
+      // bottom triangle
+      if (row > column && arraySize - row - 1 < column) {
+        trianglesSumArray[7] += array[row][column];
       }
     }
   }
 
-  cout << "top-right triangle sum: " << trianglesSumArray[0] << endl;
-  cout << "bottom-left triangle sum: " << trianglesSumArray[1] << endl;
-  cout << "top-left triangle sum: " << trianglesSumArray[2] << endl;
-  cout << "bottom-right triangle sum: " << trianglesSumArray[3] << endl;
-  cout << "big triangles sum: " << trianglesSumArray[4] << endl;
+  cout << "top-right triangle: " << trianglesSumArray[0] << endl;
+  cout << "bottom-left triangle: " << trianglesSumArray[1] << endl;
+  cout << "top-left triangle: " << trianglesSumArray[2] << endl;
+  cout << "bottom-right triangle: " << trianglesSumArray[3] << endl;
+  cout << "top triangle: " << trianglesSumArray[4] << endl;
+  cout << "left triangle: " << trianglesSumArray[5] << endl;
+  cout << "right triangle: " << trianglesSumArray[6] << endl;
+  cout << "bottom triangle: " << trianglesSumArray[7] << endl;
 
-  for (int i = 0; i < trianglesCountIncludingDiagonals; i++) {
+  for (int i = 0; i < trianglesCount; i++) {
     trianglesSumResult += trianglesSumArray[i];
   }
 
