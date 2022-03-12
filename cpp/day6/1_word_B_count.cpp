ed5bcab8339e629e19feda06519543e87db15d5e
TigranAbrahamyan <deulin666@gmail.com>
2022-03-12 18:16:51 +0400

    cpp homework day6, (word_B_count) exercise

---
+++ cpp/day6/1_word_B_count.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string sentence = "";
+  int result = 0;
+  int letterBCountInWord = 0;
+
+  cout << "Write a sentence: ";
+  getline(cin, sentence);
+
+  for (int i = 0; i < sentence.length(); i++) {
+    if (sentence[i] == 'B') {
+      letterBCountInWord++;
+    }
+
+    if (sentence[i] == ' ' || i == sentence.length() - 1) {
+      if (letterBCountInWord == 2) {
+        result++;
+      }
+
+      letterBCountInWord = 0;
+    }
+  }
+
+  cout << "Words count that contains two B letter: " << result << endl;
+
+  return 0;
+}
