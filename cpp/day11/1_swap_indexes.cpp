e1c9a4642ebf4bc20e283e1edfd874bc9e208451
TigranAbrahamyan <deulin666@gmail.com>
2022-03-21 17:01:08 +0400

    Minor change

--- cpp/day11/1_swap_indexes.cpp
+++ cpp/day11/1_swap_indexes.cpp
@@ -12,77 +12,77 @@ void getSplittedStr(string, string*, int*);
 void handleErrors(string, string*, int);
 
 int main() {
-	string num;
-	string positions;
-	string splittedStr[2] = {};
-	int splittedStrLength = 0;
+  string num;
+  string positions;
+  string splittedStr[2] = {};
+  int splittedStrLength = 0;
 
-	cout << "Input number: ";
-	cin >> num;
+  cout << "Input number: ";
+  cin >> num;
 
-	getline(cin, positions);
-	cout << "Input positions to swap: ";
-	getline(cin, positions);
+  getline(cin, positions);
+  cout << "Input positions to swap: ";
+  getline(cin, positions);
 
-	getSplittedStr(positions, splittedStr, &splittedStrLength);
-	handleErrors(num, splittedStr, splittedStrLength);
+  getSplittedStr(positions, splittedStr, &splittedStrLength);
+  handleErrors(num, splittedStr, splittedStrLength);
 
-	int firstIndex = stoi(splittedStr[0]);
-	int secondIndex = stoi(splittedStr[1]);
-	swap(num, firstIndex, secondIndex);
+  int firstIndex = stoi(splittedStr[0]);
+  int secondIndex = stoi(splittedStr[1]);
+  swap(num, firstIndex, secondIndex);
 
-	cout << "Result: " << num << endl;
+  cout << "Result: " << num << endl;
 
-	return 0;
+  return 0;
 }
 
 bool isStrDigit(string str) {
-	for (int i = 0; i < str.length(); i++) {
-		if (str[i] < '0' || str[i] > '9') {
-			return false;
-		}
-	}
+  for (int i = 0; i < str.length(); i++) {
+    if (str[i] < '0' || str[i] > '9') {
+      return false;
+    }
+  }
 
-	return true;
+  return true;
 }
 
 void swap(string &num, int firstIndex, int secondIndex) {
-	int temp = num[firstIndex];
-	num[firstIndex] = num[secondIndex];
-	num[secondIndex] = temp;
+  int temp = num[firstIndex];
+  num[firstIndex] = num[secondIndex];
+  num[secondIndex] = temp;
 }
 
 void getSplittedStr(string positions, string *splittedStr, int *splittedStrLength) {
-	for (int i = 0; positions[i] != '\0'; i++) {
-		if (*splittedStrLength > 1) {
-			break;
-		}
+  for (int i = 0; positions[i] != '\0'; i++) {
+    if (*splittedStrLength > 1) {
+      break;
+    }
 
-		if (positions[i] == ' ') {
-			*splittedStrLength += 1;
-			continue;
-		}
+    if (positions[i] == ' ') {
+      *splittedStrLength += 1;
+      continue;
+    }
 
-		splittedStr[*splittedStrLength] += positions[i];
-	}
+    splittedStr[*splittedStrLength] += positions[i];
+  }
 }
 
 void handleErrors(string num, string *splittedStr, int splittedStrLength) {
-	try {
-		if (splittedStrLength != 1) {
-			throw TWO_INDEX_NUMBER_ERROR;
-		}
+  try {
+    if (splittedStrLength != 1) {
+      throw TWO_INDEX_NUMBER_ERROR;
+    }
 
-		if (!isStrDigit(splittedStr[0]) || !isStrDigit(splittedStr[1])) {
-			throw TWO_INDEX_NUMBER_ERROR;
-		}
+    if (!isStrDigit(splittedStr[0]) || !isStrDigit(splittedStr[1])) {
+      throw TWO_INDEX_NUMBER_ERROR;
+    }
 
-		if (stoi(splittedStr[0]) >= num.length() || stoi(splittedStr[1]) >= num.length()) {
-			throw INDEX_NOT_FOUND;
-		}
-	} catch (const char* err) {
-		cout << "================================" << endl;
-		cout << "Error: " << err << endl;
-		exit(0);
-	}
+    if (stoi(splittedStr[0]) >= num.length() || stoi(splittedStr[1]) >= num.length()) {
+      throw INDEX_NOT_FOUND;
+    }
+  } catch (const char* err) {
+    cout << "================================" << endl;
+    cout << "Error: " << err << endl;
+    exit(0);
+  }
 }
