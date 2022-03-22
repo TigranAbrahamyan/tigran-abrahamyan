50788e202a872e508c73dee8e2711f2058b1fece
TigranAbrahamyan <deulin666@gmail.com>
2022-03-22 22:59:42 +0400

    Updated 'permutation' exercise

--- cpp/day11/4_permutation.cpp
+++ cpp/day11/4_permutation.cpp
@@ -1,26 +1,46 @@
 #include <iostream>
+#include <vector>
 #include <string>
 
 using namespace std;
 
+vector<string> arr = {};
+
 void findPermutations(string word, string result = "") {
   if (word.length() == 0) {
-		cout << result << endl;
-	}
+    bool shouldAddElemntToArray = true;
 
-	for (int i = 0; i < word.length(); i++) {
-		string remainingChars = word.substr(0, i) + word.substr(i + 1);
-		findPermutations(remainingChars, result + word[i]);
-	}
+    for (int i = 0; i < arr.size(); i++) {
+      if (arr[i] == result) {
+        shouldAddElemntToArray = false;
+        break;
+      }
+    }
+
+    if (shouldAddElemntToArray) {
+      arr.push_back(result);
+    }
+  } else {
+    for (int i = 0; i < word.length(); i++) {
+      string remainingChars = word.substr(0, i) + word.substr(i + 1);
+      findPermutations(remainingChars, result + word[i]);
+    }
+  }
 }
 
 int main() {
-	string word;
+  string word;
 
-	cout << "Word: ";
-	cin >> word;
+  cout << "Word: ";
+  cin >> word;
 
-	findPermutations(word, "");
+  findPermutations(word, "");
 
-	return 0;
+  for (int i = 0; i < arr.size(); i++) {
+    cout << arr[i] << endl;
+  }
+
+  cout << "Permutations count: " << arr.size();
+
+  return 0;
 }
