523360d41e824d70e884b5e83d15a1e95d7fd119
TigranAbrahamyan <deulin666@gmail.com>
2022-05-04 17:35:32 +0400

    Implemented delete method for bs_tree

--- data_structure/day07/bs_tree.h
+++ data_structure/day07/bs_tree.h
@@ -29,6 +29,7 @@ class BSTree {
   public:
     BSTree(int);
     void insert(int);
+    void deleteNode(int);
     void printSorted();
 };
 
@@ -56,11 +57,54 @@ void BSTree::insert(int value) {
 
     if (value >= current->_data) {
       current = current->_right;
-    }
-
-    if (value < current->_data) {
+    } else {
       current = current->_left;
     }
+  }
+}
+
+void BSTree::deleteNode(int value) {
+  if (!root) {
+    return;
+  }
+
+  Node* current = root;
+  Node* prev = NULL;
+
+  while (current && current->_data != value) {
+    prev = current;
+
+    if (value > current->_data) {
+      current = current->_right;
+    } else {
+      current = current->_left;
+    }
+  }
+
+  if (!current->_left || !current->_right) {
+    Node *newCurrent = current->_left ? current->_left : current->_right;
+
+    if (prev->_left == current) {
+      prev->_left = newCurrent;
+    } else {
+      prev->_right = newCurrent;
+    }
+  } else {
+    Node* temp = current->_right;
+    Node* min = NULL;
+
+    while (temp->_left) {
+      min = temp;
+      temp = temp->_left;
+    }
+
+    if (!min) {
+      current->_right = temp->_right;
+    } else {
+      min->_left = temp->_right;
+    }
+
+    current->_data = temp->_data;
   }
 }
 
@@ -68,7 +112,6 @@ void BSTree::printSorted() {
   printSorted(root);
 }
 
-// DFS
 void BSTree::printSorted(Node* tree) {
   if (!tree) {
     return;
--- data_structure/day07/main.cpp
+++ data_structure/day07/main.cpp
@@ -4,11 +4,30 @@
 using namespace std;
 
 int main() {
-  BSTree* root = new BSTree(5);
+  BSTree* root = new BSTree(3);
 
-  for (int i = 0; i < 15; i++) {
+  for (int i = 0; i < 10; i++) {
     root->insert(i);
   }
+
+  root->insert(19);
+  root->insert(25);
+  root->insert(22);
+  root->insert(23);
+  root->insert(24);
+  root->insert(30);
+  root->insert(18);
+  root->insert(14);
+  root->insert(13);
+  root->insert(40);
+  root->insert(38);
+  root->insert(34);
+  root->insert(36);
+  root->insert(35);
+
+  root->deleteNode(36);
+  root->deleteNode(9);
+  root->deleteNode(25);
 
   root->printSorted();
 
