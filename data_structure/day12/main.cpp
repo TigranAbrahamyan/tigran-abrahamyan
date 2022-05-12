5b793ffdf6c860711ccc62a74acb28fc9a5b7bb9
TigranAbrahamyan <deulin666@gmail.com>
2022-05-12 23:25:09 +0400

    data structure homework day12, implemented open addressing hash table

--- data_structure/day12/main.cpp
+++ data_structure/day12/main.cpp
@@ -1,14 +1,49 @@
+// -------separate_chaining_hash_table-------
 #include "./separate_chaining_hash_table.hpp"
 
 int main() {
   HashTable table;
   table.insert("John Smith", "123-456");
   table.insert("Smith John", "456-123");
+  table.insert("Smith John", "456-123");
+  table.insert("Smith John", "456-123");
   table.insert("Yandex Alica", "7-777-777-777");
+  table.insert("Alica Yandex", "7-777-777-777");
+  table.insert("Alica Yandex", "7-777-777-777");
+  table.insert("Alica Yandex", "7-777-777-777");
   table.remove("John Smith");
+  table.remove("Smith John");
+  table.remove("Smith John");
+  table.remove("Alica Yandex");
   table.printTable();
   cout << table.search("Yandex Alica") << endl;
   cout << table.search("Yandex") << endl;
 
   return 0;
 }
+// -------separate_chaining_hash_table-------
+
+// -------open_addressing_hash_table-------
+// #include "./open_addressing_hash_table.hpp"
+
+// int main() {
+//   HashTable table;
+//   table.insert("John Smith", "123-456");
+//   table.insert("Smith John", "456-123");
+//   table.insert("Smith John", "456-123");
+//   table.insert("Smith John", "456-123");
+//   table.insert("Yandex Alica", "7-777-777-777");
+//   table.insert("Alica Yandex", "7-777-777-777");
+//   table.insert("Alica Yandex", "7-777-777-777");
+//   table.insert("Alica Yandex", "7-777-777-777");
+//   table.remove("John Smith");
+//   table.remove("Smith John");
+//   table.remove("Smith John");
+//   table.remove("Alica Yandex");
+//   table.printTable();
+//   cout << table.search("Yandex Alica") << endl;
+//   cout << table.search("Yandex") << endl;
+
+//   return 0;
+// }
+// -------open_addressing_hash_table-------
---
+++ data_structure/day12/open_addressing_hash_table.hpp
@@ -0,0 +1,157 @@
+#ifndef OPEN_ADDRESSING_HASH_TABLE_H
+#define OPEN_ADDRESSING_HASH_TABLE_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define DEFAULT_CAPACITY 10
+
+class Table {
+  public:
+    string _key;
+    string _value;
+
+    Table(string key, string value) {
+      _key = key;
+      _value = value;
+    }
+};
+
+class HashTable {
+  private:
+    Table** _table;
+    int _capacity;
+
+    int hashFunction(string);
+    void resizeMultiply();
+    void resizeDivide();
+    int getLastPosition();
+
+  public:
+    HashTable();
+    void insert(string, string);
+    void remove(string);
+    string search(string);
+    void printTable();
+};
+
+HashTable::HashTable() {
+  _capacity = DEFAULT_CAPACITY;
+  _table = new Table*[_capacity];
+
+  for (int i = 0; i < _capacity; i++) {
+    _table[i] = NULL;
+  }
+}
+
+int HashTable::hashFunction(string key) {
+  int result = 0;
+
+  for (int i = 0; key[i] != '\0'; i++) {
+    result += key[i];
+  }
+
+  return result % _capacity;
+}
+
+void HashTable::resizeMultiply() {
+  _capacity *= 2;
+  Table** temp = new Table*[_capacity];
+
+  for (int i = 0; i < _capacity; i++) {
+    temp[i] = NULL;
+  }
+
+  for (int i = 0; i < _capacity / 2; i++) {
+    temp[i] = _table[i];
+  }
+
+  _table = temp;
+}
+
+void HashTable::resizeDivide() {
+  _capacity /= 2;
+  Table** temp = new Table*[_capacity];
+
+  for (int i = 0; i < _capacity; i++) {
+    temp[i] = NULL;
+  }
+
+  for (int i = 0; i < _capacity; i++) {
+    temp[i] = _table[i];
+  }
+
+  _table = temp;
+}
+
+int HashTable::getLastPosition() {
+  for (int i = _capacity - 1; i >= 0; i--) {
+    if (_table[i]) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+void HashTable::insert(string key, string value) {
+  int position = hashFunction(key);
+
+  if (_table[position]) {
+    while (_table[position]) {
+      position++;
+      if (position >= _capacity) {
+        resizeMultiply();
+      }
+    }
+
+    _table[position] = new Table(key, value);
+  } else {
+    _table[position] = new Table(key, value);
+  }
+}
+
+void HashTable::remove(string key) {
+  int position = hashFunction(key);
+
+  for (int i = _capacity - 1; i >= position; i--) {
+    if (_table[i] && _table[i]->_key == key) {
+      _table[i] = NULL;
+      break;
+    }
+  }
+
+  if (getLastPosition() < _capacity / 2 && _capacity != DEFAULT_CAPACITY) {
+    resizeDivide();
+  }
+}
+
+string HashTable::search(string key) {
+  int position = hashFunction(key);
+
+  if (_table[position] && _table[position]->_key == key) {
+    while (_table[position]) {
+      if (_table[position]->_key == key) {
+        return _table[position]->_value;
+      }
+
+      position++;
+    }
+  }
+
+  return "Key '" + key + "' not found";
+}
+
+void HashTable::printTable() {
+  for (int i = 0; i < _capacity; i++) {
+    cout << "[" << i << "]: ";
+    if (_table[i]) {
+      cout << "[" << _table[i]->_key << ", " << _table[i]->_value << "] ";
+    }
+    cout << endl;
+  }
+}
+
+#endif
